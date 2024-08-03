352f1e2efc173d205a05a024f9764abdf54d9fcf
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2024-08-03 12:08:13 +0530

    --rm server dir

--- server/routes/status.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { getStatusVisibleTo,getStatusOfUser,updateStatusSeenBy,deleteStatus,createStatus } from "../controllers/status.controller.js"
-const router = express.Router();
-router.get('/ofUser', getStatusOfUser);
-router.post('/create', createStatus);
-router.delete('/delete', deleteStatus);
-router.get('/visibleto/:uid', getStatusVisibleTo);
-router.patch('/updateSeenBy/:statusId', updateStatusSeenBy);
-export default router;
--- server/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import { Register ,getUserByKey} from "../controllers/user.controller.js"
-
-const router = express.Router()
-router.post("/register",Register);
-router.get("/find",getUserByKey);
-export default router;
